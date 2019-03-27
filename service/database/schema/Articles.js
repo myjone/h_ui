a6d0659ec6635617a98e489b26637608e3564ad7
zhangjie <zhangjie@yunfucloud.com>
2019-03-27 17:51:37 +0800

    'z'

--- service/database/schema/Articles.js
+++ service/database/schema/Articles.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema;
 const articleSchema = new Schema({
-	articleId:{type: mongoose.Schema.Types.ObjectId, ref:'Users'},
+	articleId:{type: mongoose.Schema.Types.ObjectId},
 	articleTitle:String,
 	articleContent:String,
 	articleImg:{type:String,default:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2066864154,87086712&fm=26&gp=0.jpg'},
